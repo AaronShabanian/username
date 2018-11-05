72e5aa500853adff10f23857fb9b9f4d0f40dc62
AaronShabanian <44658047+AaronShabanian@users.noreply.github.com>
2018-11-05 11:06:28 -0800

    Update username.sh

--- username.sh
+++ username.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 # username.sh
 #Aaron Shabanian
-echo "Enter a Username "
+echo "Enter a Username: It must start with a lowecase letter and you can only use lowercase letters, digits and the underscore character. "
 read USERNAME
 while echo $USERNAME | egrep -v "^[a-z][a-z0-9_]{3,12}$" > /dev/null 2>&1
 do
